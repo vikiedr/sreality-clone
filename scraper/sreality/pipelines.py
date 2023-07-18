863cac0e1e87952f776db62018fcda59f0ac9a9a
Vasek Mracek <mracekv@gmail.com>
2023-07-18 11:41:34 +0200

    save scraped data to db

--- scraper/sreality/pipelines.py
+++ scraper/sreality/pipelines.py
@@ -5,9 +5,30 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import psycopg2
+import os
 
 
 class SrealityPipeline:
+    def __init__(self):
+        hostname = 'db'
+        username = os.environ.get('POSTGRES_USER')
+        password = os.environ.get('POSTGRES_PASSWORD')
+        database = os.environ.get('POSTGRES_NAME')
+        
+        self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+        
+        self.cur = self.connection.cursor()
+        
     def process_item(self, item, spider):
+        self.cur.execute(""" insert into listing_flat (title, img_url) values (%s,%s)""", (
+            item["title"],
+            item["img_url"]
+        ))
+
+        self.connection.commit()
         return item
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.connection.close()
